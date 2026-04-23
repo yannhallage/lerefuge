f378e35a5f9563961cb06b9b2e21c6f0058763d2
Yann Hallage <contact.devhllg@gmail.com>
2026-04-23 19:20:54 +0000

    update

--- form.js
+++ form.js
@@ -1,63 +1,45 @@
 
-$(function()
-{
-    function after_form_submitted(data) 
-    {
-        if(data.result == 'success')
-        {
-            $('form#contact_form').hide();
-            $('#success_message').show();
-            $('#error_message').hide();
-        }
-        else
-        {
-            $('#error_message').append('<ul></ul>');
-
-            jQuery.each(data.errors,function(key,val)
-            {
-                $('#error_message ul').append('<li>'+key+':'+val+'</li>');
-            });
-            $('#success_message').hide();
-            $('#error_message').show();
-
-            //reverse the response on the button
-            $('button[type="button"]', $form).each(function()
-            {
-                $btn = $(this);
-                label = $btn.prop('orig_label');
-                if(label)
-                {
-                    $btn.prop('type','submit' ); 
-                    $btn.text(label);
-                    $btn.prop('orig_label','');
-                }
-            });
-            
-        }//else
-    }
-
-	$('#contact_form').submit(function(e)
-      {
+$(function () {
+    $("#contact_form").submit(function (e) {
         e.preventDefault();
 
-        $form = $(this);
-        //show some response on the button
-        $('button[type="submit"]', $form).each(function()
-        {
-            $btn = $(this);
-            $btn.prop('type','button' ); 
-            $btn.prop('orig_label',$btn.text());
-            $btn.text('Sending ...');
-        });
-        
+        var hasBookingFields = $("#date-picker").length > 0 && $("#room-type").length > 0;
+        var date = $("#date-picker").val();
+        var adults = $(".guests-n-rooms .col-md-4").eq(0).find("input").val();
+        var children = $(".guests-n-rooms .col-md-4").eq(1).find("input").val();
+        var rooms = $("#room-count").val();
+        var roomType = $("#room-type").val();
+        var name = $("#name").val();
+        var email = $("#email").val();
+        var phone = $("#phone").val();
+        var messageText = $("#message").val();
 
-                    $.ajax({
-                type: "POST",
-                url: 'handler.php',
-                data: $form.serialize(),
-                success: after_form_submitted,
-                dataType: 'json' 
-            });        
-        
-      });	
+        var message = "";
+
+        if (hasBookingFields) {
+            message =
+                "Bonjour, je souhaite faire une reservation au Refuge du Bandama.\n\n" +
+                "Date: " + (date || "-") + "\n" +
+                "Adultes: " + (adults || "-") + "\n" +
+                "Enfants: " + (children || "-") + "\n" +
+                "Chambres: " + (rooms || "-") + "\n" +
+                "Type de chambre: " + (roomType || "-") + "\n\n" +
+                "Nom: " + (name || "-") + "\n" +
+                "Email: " + (email || "-") + "\n" +
+                "Telephone: " + (phone || "-") + "\n" +
+                "Message: " + (messageText || "-");
+        } else {
+            message =
+                "Bonjour, je vous contacte depuis le formulaire du site.\n\n" +
+                "Nom: " + (name || "-") + "\n" +
+                "Email: " + (email || "-") + "\n" +
+                "Telephone: " + (phone || "-") + "\n" +
+                "Message: " + (messageText || "-");
+        }
+
+        const phoneNumber = "+2250748354479";
+        const whatsappURL = `https://api.whatsapp.com/send?phone=${phoneNumber}&text=${encodeURIComponent(message)}`;
+
+        window.open(whatsappURL, "_blank");
+    });
 });
